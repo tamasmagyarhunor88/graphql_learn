a11a4cd665ebe04a66f3192a408f543cc7dde0b9
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2022-01-09 21:49:03 +0000

    Add basic, hardcoded schema, update index

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-import express from 'express';
+import express from "express";
+import schema from "./schema";
+import { graphqlHTTP } from 'express-graphql';
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('GraphQL is amazing!');
 });
+
+const root = { hello: () => "Hi I'm Manny" };
+
+app.use('/graphql', graphqlHTTP ({
+    schema: schema,
+    rootValue: root,
+    graphiql: root
+}));
 
 app.listen(8080, () => console.log('Running on server port localhost:8080/graphql'));
