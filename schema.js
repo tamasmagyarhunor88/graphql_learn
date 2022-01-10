95c1a3f00717dd5a5d689fe1a7587bf60232d184
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2022-01-10 20:15:06 +0000

    Refactor code to GraphQL syntax

--- schema.js
+++ schema.js
@@ -1,6 +1,7 @@
-import { buildSchema} from 'graphql';
+import { makeExecutableSchema} from 'graphql-tools';
+import { resolvers } from "./resolvers";
 
-const schema = buildSchema(`
+const typeDefs = `
     type Friend {
         id: ID
         firstName: String
@@ -44,6 +45,8 @@ const schema = buildSchema(`
     type Mutation {
         createFriend(input: FriendInput): Friend
     }
-`)
+`;
 
-export default schema;
+const schema = makeExecutableSchema({typeDefs, resolvers});
+
+export { schema };
