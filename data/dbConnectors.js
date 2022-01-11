813b6b4786292b89f7951c7607b442f0178f16f6
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2022-01-11 20:58:23 +0000

    Fix storage and sqlite db path

--- data/dbConnectors.js
+++ data/dbConnectors.js
@@ -39,13 +39,23 @@ const Friends = mongoose.model('friends', friendSchema);
 // SQL
 const sequelize = new Sequelize('database', null, null, {
     dialect: 'sqlite',
-    store: '/alien.sqlite',
+    storage: './alien.sqlite',
 });
 
 const Aliens = sequelize.define('aliens', {
     firstName: { type: Sequelize.STRING },
     lastName: { type: Sequelize.STRING },
     planet: { type: Sequelize.STRING },
+});
+
+Aliens.sync({ force: true }).then(() => {
+    _.times(10, (i) => {
+        Aliens.create({
+            firstName: casual.first_name,
+            lastName: casual.last_name,
+            planet: casual.word
+        });
+    });
 });
 
 
